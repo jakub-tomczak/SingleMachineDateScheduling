22879edcdc1dd4e2a1f50d1d137805244ce5eecf
Jakub Tomczak <jakub.tomczak@live.com>
2018-10-06 16:37:16 +0200

    Sort by a length to earliness cost ratio descending.

--- SingleMachineDateScheduling/heuristics.py
+++ SingleMachineDateScheduling/heuristics.py
@@ -8,13 +8,12 @@ def tradeOffMethod(arguments):
     
     #calculate penalties
     #for an earliness
-    instance[:,4] = (dueDate - instance[:,0])*instance[:,1]
+    instance[:,4] = instance[:,0] / instance[:,1]
     #for a tardiness
-    dueDateToEnd = tasksLength - dueDate
-    instance[:,5] = dueDateToEnd*instance[:,2]
+    instance[:,5] = instance[:,0] / instance[:,2]
     
-    earlinessArray = np.array(sorted(instance, key=lambda task: task[4])) #sort by the earliness penalties
-    tardinessArray = np.array(sorted(instance, key=lambda task: task[5])) #sort the tardiness penalties
+    earlinessArray = np.array(sorted(instance, key=lambda task: task[4], reverse=True)) #sort by the earliness coeff
+    tardinessArray = np.array(sorted(instance, key=lambda task: task[5], reverse=True)) #sort by the tardiness coeff
     print(earlinessArray)
     print(tardinessArray)
     currentLength = 0
@@ -23,8 +22,8 @@ def tradeOffMethod(arguments):
     iter = 0
 
     #what task should we take in the current iteration?
-    earlinessIndex = 0
-    tardinessIndex = 0
+    #earlinessIndex = 0
+    #tardinessIndex = 0
 
     #check wheter the current best task is not exceeding dueDate limit (in both direction - depending on the stage)
     canAddAnotherTask = True
@@ -40,28 +39,31 @@ def tradeOffMethod(arguments):
             #tasks that will fit before dueDate
             #x[4] - earliness penalty when adding this element in the current iteration
             #x[6]==1 task has been already assigned
-            bestTask = earlinessArray[earlinessIndex]
+
+            #take only not assigned tasks
+            earlinessArray = np.array([x for x in earlinessArray if x[6]!=1 and x[0]+currentLength <= dueDate])
             
-            if bestTask[4] < 0:
+            #found no candidates for the bestTask, continue in tardiness stage
+            if len(earlinessArray) == 0:
                 canAddAnotherTask = False
+                iter +=1
                 continue
+            bestTask = earlinessArray[0]
             
             print(iter, ' > ',currentLength, bestTask, currentCost)
-            earlinessIndex+=1
             #task used - assign its global id (to be found in tardinessArray)
             tasksAssigned.append(int(bestTask[3])) #[3] -> index
             #and mark it as used
             bestTask[6] = 1 #TODO rewrite it, in order not to use a number as a flag -> it's float!
             #prolong already assigned tasks length
             currentLength += bestTask[0]
-            currentCost += bestTask[4]
-            earlinessArray[:,4] -= bestTask[0]*earlinessArray[:,1]
+            currentCost += (dueDate - currentLength)*bestTask[1]
         else:
             print(iter, 'tardiness stage')
             #prefer tardiness - tardiness stage
             if not recalculatedTardinessArray:
                 #take only not assigned elements
-                tardinessArray = [x for x in tardinessArray if x[6]!=1] 
+                #tardinessArray = [x for x in tardinessArray if x[6]!=1] -> done when taking new tardinessArray : 69
                 # earliness may have ended up with currentLength lower than dueDate
                 # recalculating is needed to add proper tardiness penalties
                 # however, additional sorting is not necessary
@@ -70,24 +72,24 @@ def tradeOffMethod(arguments):
                 tardinessArray[:, 5] = (dueDate - currentLength)*tardinessArray[:,2] #[:,2] -> tardiness coeff
                 recalculatedTardinessArray = True 
             
-            bestTask = tardinessArray[tardinessIndex]
+            tardinessArray = np.array([x for x in tardinessArray if x[6]!=1 and x[0]+currentLength <= dueDate])
             
-            #check wheter the current best task is not exceeding dueDate limit 
-            if bestTask[4] < 0:
+            #found no candidates for the bestTask, continue in earliness stage
+            if len(tardinessArray) == 0:
                 canAddAnotherTask = False
-                #TODO check whether algorithm has been already in the earliness stage
-                continue #may enter earliness stage in the next iteration, so don't break
+                iter += 1
+                continue
+            
+            bestTask = tardinessArray[0]
             
             print(iter, ' > ',currentLength, bestTask, currentCost)
-            tardinessIndex+=1
             #task used - assign its global id (to be found in tardinessArray)
             tasksAssigned.append(int(bestTask[3])) #[3] -> index
-            #prolong used tasks length
+            #and mark it as used
+            bestTask[6] = 1 #TODO rewrite it, in order not to use a number as a flag -> it's float!
+            #prolong already assigned tasks length
             currentLength += bestTask[0]
-            currentCost += bestTask[5]
-
-            tardinessArray[:,5] -= bestTask[0]*earlinessArray[:,1]
-
+            currentCost += (currentLength+bestTask[0]-dueDate)*bestTask[1]
         iter += 1
     print('ended on iteration {}, length {}, dueDate {}, cost {}'.format(iter, currentLength, dueDate, currentCost))
     return 0
