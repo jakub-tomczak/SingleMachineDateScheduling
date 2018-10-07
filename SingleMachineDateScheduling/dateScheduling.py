fdd13c13373402b20b091528cddbc03d4ccaed00
Jakub Tomczak <jakub.tomczak@live.com>
2018-10-07 10:16:05 +0200

    Run many instances with one invocation.

--- SingleMachineDateScheduling/dateScheduling.py
+++ SingleMachineDateScheduling/dateScheduling.py
@@ -5,14 +5,7 @@ from methodRunner import invokeMethod
 from IOmanager import getTest, dumpResults
 from heuristics import tradeOffMethod
 
-def main(): 
-    arguments = { 
-            'k' : 1,    #which test from test is going to be used
-            'n' : 10,   #what is the instance size
-            'h' : 0.6,
-            'method' : tradeOffMethod
-    }
-    validateInput(arguments)
+def instanceRunner(arguments):
     arguments['instance'] = getTest(n = arguments['n'], k = arguments['k'])
     methodToInvoke = arguments['method']
 
@@ -26,6 +19,20 @@ def main():
     else:
         print('Execution time is {:0.20f}'.format( result['time']) )
 
+
+def main(): 
+    arguments = { 
+            'k' : 1,    #which test from test is going to be used
+            'n' : 20,   #what is the instance size
+            'h' : 0.8,
+            'method' : tradeOffMethod
+    }
+    validateInput(arguments)
+    for k in range(10):
+        arguments['k'] = k
+        for h in [0.6, .8]:
+            arguments['h'] = h
+            instanceRunner(arguments)
      
 def validateInput(arguments):
     assert len(options.testsDirectory) > 0 and len(options.outputDirectory) > 0, 'Check tests or output directory (cannot be empty)'
