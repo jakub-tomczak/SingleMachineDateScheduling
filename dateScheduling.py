afa368bab56cb6c04f2c9cf05ceca6a1a86987b4
Jakub Tomczak <jakub.tomczak@live.com>
2018-10-19 10:48:59 +0200

    Run methods from command line.

--- dateScheduling.py
+++ dateScheduling.py
@@ -1,6 +1,7 @@
 import os
 from options import *
 import numpy as np
+import argparse
 from methodRunner import invokeMethod
 from IOmanager import getTest, dumpResults
 from heuristics import tradeOffMethod
@@ -24,25 +25,43 @@ def instanceRunner(arguments):
     else:
         print('Execution time is {:0.20f}'.format( result['time']) )
 
+def parseArguments():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('n', type=int,
+                        help='Number of tasks from set [10,20,50,100,200]')
+    parser.add_argument('k', type=int,
+                        help='Instance index, natural number in range <1,10>')
+    parser.add_argument('h', type=float,
+                        help='Used to calculate due date, real number in range <0,1>')
+    parser.add_argument('index', type=int, default=0,
+                        help='Student\'s index')
+    args = parser.parse_args()
 
-def main(): 
-    arguments = { 
-            'k' : 1,    #which test from test is going to be used
-            'n' : 20,   #what is the instance size
-            'h' : 0.8,
-            'method' : tradeOffMethod
+    arguments = {
+        'n' : args.n,
+        'k' : args.k,
+        'h' : args.h,
+        'studentsIndex' : args.index
     }
+    return arguments
+
+
+def main(arguments): 
+    arguments['method'] = tradeOffMethod
     validateInput(arguments)
-    for k in range(10):
-        arguments['k'] = k
-        for h in [0.6, .8]:
-            arguments['h'] = h
-            instanceRunner(arguments)
+    instanceRunner(arguments)
+    #for k in range(10):
+    #    arguments['k'] = k
+    #    for h in [0.6, .8]:
+    #        arguments['h'] = h
+    #        instanceRunner(arguments)
      
 def validateInput(arguments):
-    assert len(options.testsDirectory) > 0 and len(options.outputDirectory) > 0, 'Check tests or output directory (cannot be empty)'
     assert arguments['method'], 'Method to invoke cannot be empty.'
-    assert arguments['k'] >= 0 and arguments['k'] < 10, 'There are only 10 tests for each instance available'
+    assert arguments['k'] > 0 and arguments['k'] <= 10, 'k={} is not valid. There are only 10 tests for each instance available'.format(arguments['k'])
+    assert arguments['n'] in [10,20,50, 100, 200, 500, 1000], 'n={} is not valid. It must be a number from the set [10, 20, 50, 100, 200, 500, 1000]'.format(arguments['n'])
+    assert arguments['h'] >= 0 and arguments['h'] <=1, 'h={} is not valid. h must be in range <0, 1>.'
     
 if __name__ == '__main__':
-    main()
+    arguments = parseArguments()
+    main(arguments)
