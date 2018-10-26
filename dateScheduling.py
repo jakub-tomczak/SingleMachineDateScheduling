9825448b66c5a2491efdf6ed7df3eb96d9e92af8
Jakub Tomczak <jakub.tomczak@live.com>
2018-10-26 11:53:08 +0200

    Update arguments properties.

--- dateScheduling.py
+++ dateScheduling.py
@@ -18,28 +18,28 @@ def instanceRunner(arguments):
     result = invokeMethod(methodToInvoke, arguments)
 
     if result['returnedValue'] != None and options.printResultToStdout:
-        print('Time is {:0.9f} the result: {}'.
-            format(result['time'], result['returnedValue']))
+        print('Time is {:0.9f} the result (cost): {}'.
+            format(result['time'], result['returnedValue'][3]))
         if options.dumpResults:
             dumpResults(arguments, result, options.dumpFormat)
     else:
         print('Execution time is {:0.20f}'.format( result['time']) )
 
 def parseArguments():
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('n', type=int,
+    parser = argparse.ArgumentParser(description='', add_help=False)
+    parser.add_argument('-n', type=int,
                         help='Number of tasks from set [10,20,50,100,200]')
-    parser.add_argument('k', type=int,
+    parser.add_argument('-k', type=int,
                         help='Instance index, natural number in range <1,10>')
-    parser.add_argument('h', type=float,
+    parser.add_argument('-h', type=float,
                         help='Used to calculate due date, real number in range <0,1>')
-    parser.add_argument('index', type=int, default=0,
+    parser.add_argument('-index', type=str, default=0,
                         help='Student\'s index')
     args = parser.parse_args()
 
     arguments = {
         'n' : args.n,
-        'k' : args.k,
+        'k' : args.k - 1,   #k loaded from file should be in range <0,9>
         'h' : args.h,
         'studentsIndex' : args.index
     }
@@ -58,10 +58,11 @@ def main(arguments):
      
 def validateInput(arguments):
     assert arguments['method'], 'Method to invoke cannot be empty.'
-    assert arguments['k'] > 0 and arguments['k'] <= 10, 'k={} is not valid. There are only 10 tests for each instance available'.format(arguments['k'])
+    assert arguments['k'] >= 0 and arguments['k'] < 10, 'k={} is not valid. There are only 10 tests for each instance available'.format(arguments['k'])
     assert arguments['n'] in [10,20,50, 100, 200, 500, 1000], 'n={} is not valid. It must be a number from the set [10, 20, 50, 100, 200, 500, 1000]'.format(arguments['n'])
     assert arguments['h'] >= 0 and arguments['h'] <=1, 'h={} is not valid. h must be in range <0, 1>.'
     
 if __name__ == '__main__':
     arguments = parseArguments()
     main(arguments)
+    exit(0)
