2d7a8b28f25316cc0422cdac1d5be56cc2d78638
Jakub Tomczak <jakub.tomczak@live.com>
2018-10-28 21:26:31 +0100

    Update options, add best result to instance, extend result class.

--- options.py
+++ options.py
@@ -3,21 +3,34 @@ class options:
         self.method = None
         self.printResultToStdout = True
         self.testsDirectory = 'instances'
-        self.outputDirectory = 'output'
+        self.outputDirectory = ''
         self.debugDirectory = 'out_debug'
         self.instancesSizes = [10,20,50,100,200,500,1000]
-        self.dumpResults = True
+        self.dumpResults = False
         self.dumpFormat = "txt"
-        self.debug = True  #dump debug to a file
+        self.txtFilename = "dupa"
+        self.debugFilename = "DEBUG_second"
+        self.debug = True #dump debug to a file
         self.verboseDebug = False #print text during debug
+        self.bestResultsFilename = "best_results.txt"
+        self.dumpBatchRunner = False
+        self.batchRunnerFilename = "batch"
 
 class instance:
-    def __init__(self, n, k, h, index =''):
+    index = '127083'
+
+    def __init__(self, n, k, h):
         self.n = n
         self.k = k
         self.h = h
-        self.index = index
         self.data = None
+        self.best_cost = 0
+        self.best_cost_is_optimal = False
+
+    def __eq__(self, other):
+        if isinstance(other, instance):
+            return self.n == other.n and self.k == other.k and self.h == other.h
+        return False
 
 class result:
     def __init__(self, instance):
@@ -28,3 +41,4 @@ class result:
         self.totalLength = 0
         self.dueDate = 0
         self.instance = instance
+        self.is_solution_feasible = False
