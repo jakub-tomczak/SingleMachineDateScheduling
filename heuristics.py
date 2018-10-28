709d40e9edf19b07d52ea3d10846f140033b4a15
Jakub Tomczak <jakub.tomczak@live.com>
2018-10-28 21:31:23 +0100

    Fix some issues with cost calculations.

--- heuristics.py
+++ heuristics.py
@@ -1,38 +1,39 @@
 import numpy as np
 from options import result
+from validator import calculate_ordering_cost
 def tradeOffMethod(instance, debugPrinter):
-    np.set_printoptions(precision=2)
+    np.set_printoptions(precision=3)
     printer = debugPrinter
     methodResult = result(instance)
     #arguments to local vars
     tasksLength = sum([x[0] for x in instance.data]) #sum by index 0 => task's length
     methodResult.dueDate = int(instance.h*tasksLength)
-    
+
     #calculate length/penalty ratios
     #for an earliness
     instance.data[:, 4] = instance.data[:,0] / instance.data[:,1]
     #for a tardiness
     instance.data[:, 5] = instance.data[:,0] / instance.data[:,2]
-    
+
     earlinessArray = np.array(sorted(instance.data, key=lambda task: task[4], reverse=True)) #sort by the earliness ratio
     tardinessArray = np.array(sorted(instance.data, key=lambda task: task[5], reverse=True)) #sort by the tardiness ratio
-    printer.print(instance.data[:, :6])
+    #printer.print(instance.data[:, :6])
     #iteration of the algorithm
     iter = 0
-    
+
     #flag that indicates whether we've recalculated tardiness penalties and removed already assigned tasks (in the earliness stage)
     #if h>.5 we start from earliness stage so recalculating earlinessArray is not necessary
     recalculatedTardinessArray = instance.h<.5
     recalculatedEarlinessArray = instance.h>.5
-    
+
     #indices of assigned tasks
     tasksAssigned = []
     #tardiness should be reversed
     tardinessAssigned = []
-    
+
     #do we start from the earliness stage?
     earlinessStage = instance.h>.5
-    
+
     tardinessDueDate = tasksLength - methodResult.dueDate
     currentTardinessLength = 0
 
@@ -42,14 +43,14 @@ def tradeOffMethod(instance, debugPrinter):
                 #take only not assigned elements
                 earlinessArray = np.array([x for x in earlinessArray if x[3] not in tardinessAssigned])
                 printer.print('earliness: assigned {}, to assign {}'.format(tasksAssigned, earlinessArray[:,3]))
-                recalculatedEarlinessArray = True 
+                recalculatedEarlinessArray = True
             #x[4] - task's length to earliness penalty ratio
             #x[6]==1 task has been already assigned
 
             #take only not assigned tasks
-            earlinessArray = np.array([x for x in earlinessArray if x[6]!=1 and 
-                (instance.h<0.5 or x[0]+methodResult.length <= methodResult.dueDate) ]) #if h<.5 then earliness stage is the second one so we don't care about the limit 
-            
+            earlinessArray = np.array([x for x in earlinessArray if x[6]!=1 and
+                (instance.h<0.5 or x[0]+methodResult.length <= methodResult.dueDate) ]) #if h<.5 then earliness stage is the second one so we don't care about the limit
+
             #found no candidates for the bestTask, continue in tardiness stage
             if len(earlinessArray) == 0:
                 earlinessStage = False
@@ -70,7 +71,7 @@ def tradeOffMethod(instance, debugPrinter):
             else:
                 lengthToDueDate = methodResult.dueDate - (methodResult.length - currentTardinessLength)
             costToAdd = lengthToDueDate*bestTask[1]
-            methodResult.cost += costToAdd 
+            methodResult.cost += costToAdd
             printer.print('adding {}*{}'.format(lengthToDueDate, bestTask[1]))
             printer.print('iter {} -> earliness: chosen task id={}, x:{},a:{}, length {}, dueDate {}, cost {}, added:{}'.
                 format(iter, bestTask[3], bestTask[0], bestTask[1], methodResult.length, methodResult.dueDate, methodResult.cost, costToAdd))
@@ -79,32 +80,32 @@ def tradeOffMethod(instance, debugPrinter):
             if not recalculatedTardinessArray:
                 #take only not assigned elements
                 tardinessArray = np.array([x for x in tardinessArray if x[3] not in tasksAssigned])
-                recalculatedTardinessArray = True 
+                recalculatedTardinessArray = True
                 printer.print('tardiness: assigned {}, to assign {}'.format(tasksAssigned, tardinessArray[:,3]))
-            
-            tardinessArray = np.array([x for x in tardinessArray if x[6]!=1 and 
+
+            tardinessArray = np.array([x for x in tardinessArray if x[6]!=1 and
                ( instance.h>0.5 or (tasksLength-methodResult.dueDate)-methodResult.length >=0) ]) #if h>0.5 then we don't care about the size - earliness stage was first
                #otherwise check if current task is not longer than the total time for tardiness stage
-            
+
             #found no candidates for the bestTask, continue in earliness stage
             if len(tardinessArray) == 0:
                 #TODO check if earliness stage has been performed already in order to finish
                 earlinessStage = True
                 iter += 1
                 continue
-            
+
             bestTask = tardinessArray[0]
             tardinessAssigned.append(int(bestTask[3])) #[3] -> index
             bestTask[6] = 1 #TODO rewrite it, in order not to use a number as a flag -> it's float!
-            
-            #for the first task currentTardinessLength - we count the penalty based on the termination time
-            if methodResult.length < methodResult.dueDate and currentTardinessLength == 0 and iter > 1:
-                lengthToDueDate = tasksLength - (methodResult.length + bestTask[0]) - (methodResult.dueDate - methodResult.length)
-            else:
-                #tardiness phase was first
-                lengthToDueDate = tardinessDueDate - currentTardinessLength
 
-            methodResult.length += bestTask[0]            
+            #for the first task currentTardinessLength - we count the penalty based on the termination time
+            #if methodResult.length < methodResult.dueDate and currentTardinessLength == 0 and iter > 1:
+            #    lengthToDueDate = tasksLength - (methodResult.length + bestTask[0]) - (methodResult.dueDate - methodResult.length)
+            #else:
+            #    #tardiness phase was first
+            lengthToDueDate = tardinessDueDate - currentTardinessLength
+
+            methodResult.length += bestTask[0]
             currentTardinessLength += bestTask[0]
             costToAdd = lengthToDueDate*bestTask[2]
             methodResult.cost += costToAdd
@@ -114,11 +115,10 @@ def tradeOffMethod(instance, debugPrinter):
                 .format(iter, bestTask[3], bestTask[0], bestTask[2], methodResult.length, methodResult.dueDate, methodResult.cost, costToAdd))
 
         iter += 1
-    
+
     tardinessAssigned.reverse()
     methodResult.order = tasksAssigned + tardinessAssigned
-    
-    printer.print("assigned tasks {}".format(tasksAssigned))
+    printer.print("assigned tasks {}".format(methodResult.order))
     #if arguments['n'] > 20:
     #    tasksAssigned = []
     
